69fc85c0e0850f563157a621531867e0cb24595c
Enrique Mayo <jmayo@unitedvirtualities.com>
2019-12-02 12:55:45 -0700

    Added users route

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ app.get('/', function(req, res){
     res.send("Hello World!");
 })
 
+app.get('/users', function(req, res){
+    res.json({
+        user : {}
+    })
+})
+
+
 app.listen(3000, function(){
     console.log("server is listening");
 })
