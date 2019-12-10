3cb2fa7a40f1cb551313b7a2fc674eeb3eb30428
Enrique Mayo <jmayo@unitedvirtualities.com>
2019-12-09 20:33:54 -0700

    step-3-fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
-var users = {}
+app.use(express.json())
+
+var users = [{id: 1, name: 'Hakim' }]
 
 app.get('/', function(req, res){
     res.send("Hello World!");
@@ -11,18 +13,18 @@ app.get('/users', function(req, res){
     res.send(users)
 })
 
-app.post('/users/:id', function(req, res){
-    idUser = users.length;
+app.post('/user/:id', function(req, res){
+    //idUser = users.length;
     var newUser = {
-        id: idUser,
-        name: req.body.userName
+        id: req.params.id,
+        name: req.body.name
     }  
     users.push(newUser)
     res.status(201).send(newUser)
 })
 
-app.get('/users/:id', function(req, res){
-    const user = users.find(user => user.id === req.params.id)
+app.get('/user/:id', function(req, res){
+    const user = users.find(user => user.id == req.params.id)
     if(!user){
         res.status(404)
            .send('No se encontró el usuario indicado')
@@ -31,19 +33,16 @@ app.get('/users/:id', function(req, res){
     }
 })
 
-app.post('/users/:id', function(req, res){
-    newUser = {id: req.params.id};
-    users.push(newUser)
-    res.send(users)
-    res.status(200)
-})
-
-app.get('/users/:id', function(req, res){
-    newUser = {id: req.params.id};
-    
-    users.push(newUser)
-    res.send(users)
-
+app.delete('/user/:id', function(req, res){
+    const user = users.find(user => user.id == req.params.id)
+    if(!user){
+        res.status(404)
+           .send('No se encontró el usuario indicado')
+    } else {
+        const index = users.indexOf(user)
+        users.splice(index,1)
+        res.status(200).send('Usuario borrado')
+    }
 })
 
 app.listen(3000, function(){
