166dc34d462b61c4532a6c1b409e4bcd9b1ffbfc
Enrique Mayo <jmayo@unitedvirtualities.com>
2019-12-10 11:17:12 -0700

    Fix 46

--- src/index.js
+++ src/index.js
@@ -5,18 +5,44 @@ app.use(express.json())
 
 var users = []
 
+//Create a Server that would answer to "/" with "Hello World"
 app.get('/', function(req, res){
     res.send("Hello World!");
 })
 
+//Create an endpoint that would answer to GET to /users with the list of users
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+
 app.post('/user', function(req, res){
-    res.status(200).send(users.id)
+    users.push({id:0})
+    res.json(users);
 })
 
-app.get('/users', function(req, res){
-    res.send(users)
+//Create an endpoint to GET to /user/:id
+app.get('/user/:id', function(req, res){
+    //idUser = users.length;
+    var newUser = {
+        id: parseInt(req.params.id),
+        //name: req.body.name
+    }  
+    users.push(newUser)
+    res.status(200).send(newUser)
+
+
+
+    // const user = users.find(user => user.id == req.params.id)
+    // if(!user){
+    //     res.status(404)
+    //        .send('No se encontró el usuario indicado')
+    // } else {
+        //res.send(users)
+    // }
 })
 
+
+//Create an endpoint to POST to /user/:id
 app.post('/user/:id', function(req, res){
     //idUser = users.length;
     var newUser = {
@@ -24,19 +50,10 @@ app.post('/user/:id', function(req, res){
         //name: req.body.name
     }  
     users.push(newUser)
-    res.status(200).send(newUser)
+    res.status(200).send(users.id)
 })
 
-app.get('/user/:id', function(req, res){
-    const user = users.find(user => user.id == req.params.id)
-    if(!user){
-        res.status(404)
-           .send('No se encontró el usuario indicado')
-    } else {
-        res.send(users)
-    }
-})
-
+//Create an endpoint to DELETE to /user/:id
 app.delete('/user/:id', function(req, res){
     const user = users.find(user => user.id == req.params.id)
     if(!user){
