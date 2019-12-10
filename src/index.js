7b3f239f4ec1403e07708a7a464d79f7ac5c9d0a
Enrique Mayo <jmayo@unitedvirtualities.com>
2019-12-09 19:20:16 -0700

    step-3-fixed3

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
 const express = require('express');
 const app = express();
 
-const users = []
+var users = []
 
 app.get('/', function(req, res){
     res.send("Hello World!");
 })
 
 app.get('/users', function(req, res){
-
-    res.send(
-            users
-    );
+    res.send(users)
 })
+
+app.get('/users/:id', function(req, res){
+    const user = users.find(user => user.id === req.params.id)
+    if(!user){
+        res.status(404)
+           .send('No se encontró el usuario indicado')
+    } else {
+        res.send(users)
+    }
+})
+
+app.post('/users/:id', function(req, res){
+    idUser = users.length;
+    var newUser = {
+        id: idUser,
+        name: req.body.userName
+    }  
+    users.push(newUser)
+    res.status(201).send(newUser)
+})
+
 
 app.listen(3000, function(){
     console.log("server is listening");
